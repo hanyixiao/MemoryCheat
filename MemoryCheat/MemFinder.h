e4e4225fe4e323a50ce8b17d9f3032c9d12287d6
han <2456181875@qq.com>
2019-03-23 16:05:22 +0800

    2019-3-23

--- MemoryCheat/MemFinder.h
+++ MemoryCheat/MemFinder.h
@@ -50,6 +50,58 @@ public:
 		m_arList.clear();
 		return _FindFirst();
 	}
+	/*
+	*在findFirst的基础上继续寻找
+	*/
+	template< typename T>
+	bool FindNext(T value)
+	{
+		return _FindNext(value);
+	}
+	//读内存
+	template< typename T>
+	bool Read(DWORD dwAddr, T &val)
+	{
+		if (::ReadProcessMemory(m_hProcess, (LPCVOID)dwAddr, &val, sizeof(val), NULL)) {
+			return true;
+		}
+		return false;
+	}
+	bool Read(DWORD dwAddr, unsigned char val[], size_t len)
+	{
+		if (::ReadProcessMemory(m_hProcess, (LPCVOID)dwAddr, &val[0], len, NULL)) {
+			return true;
+		}
+		return false;
+	}
+	//写内存
+	template< typename T>
+	bool Write(DWORD dwAddr, T val)
+	{
+		if (::WriteProcessMemory(m_hProcess, (LPVOID)dwAddr, &val, sizeof(val), NULL)) {
+			return true;
+		}
+		return false;
+	}
+	bool Write(DWORD dwAddr, unsigned char code[], size_t len)
+	{
+		if (::WriteProcessMemory(m_hProcess, (LPVOID)dwAddr, &code[0], len, NULL)) {
+			return true;
+		}
+		return false;
+	}
+	//调用回调函数
+	bool RemoteCall(unsigned char code[], size_t len,
+		unsigned para[], size_t paraLen)
+	{
+		if (!IsValidHandle()) {
+
+		}
+		//申请代码内存
+		PVOID mFuncAddr = ::VirtualAllocEx(m_hProcess, NULL, len,
+			MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+	}
 private:
 	//关闭进程句柄
 	void SafeCloseHandle()
@@ -77,9 +129,63 @@ private:
 		const size_t len = sizeof(value);
 		const void *pValue = &value;
 	}
+	template< typename T>
+	bool _FindNext(T value)
+	{
+		//目标值的长度
+		const size_t len = sizeof(value);
+		const void *pValue = &value;
+
+		//已经储存的地址数量
+		size_t cnt = m_arlist.size();
+		size_t index = 0;
+		std::list<DWORD> dwTemp;
+		for (auto addr : m_arlist) {
+			//如果有回调函数，并且调用函数返回FALSE，则退出查找
+			if (this->m_pGoonNext && !this->m_pGoonNext(this->m_pArgsNext, cnt, index++))
+			{
+				return false;
+			}
+			//处理消息
+			WaitForIdle();
+
+			//读取内容
+			T tReadValue;
+			if (!::ReadProcessMemory(m_hProcess, (LPCVOID)addr, &tReadValue, len, NULL)
+			{
+				continue;//没有读取成功
+			}
+			//和目标进行对比
+			if (0 == memcmp(pValue, &tReadValue, len)) {
+				//值相等保存
+				dwTemp.push_back(addr);
+			}
+		}
+		//保存本次结果
+		m_arlist = dwTemp;
+		return !m_arlist.empty();
+	}
+	//看看有没有消息，有就取出来，并且分发下去、
+	void WaitForIdle()
+	{
+		MSG msg;
+		while (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {//hWnd,参数==nULL 则获取所有句柄的消息
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+		}
+	}
 	DWORD m_dwPageSize;
 	//目标进程句柄
 	HANDLE m_hProcess{ INVALID_HANDLE_VALUE };
 	//搜索的结果
 	std::list<DWORD>m_arlist;
+	//回调函数
+	typedef bool(_stdcall *PFUN_CALLBACK)(void *pArgs, size_t nPageCount, size_t index);
+	//首次扫描 回调函数
+	PFUN_CALLBACK m_pGoonFirst{ nullptr };
+	//下次扫描 回调函数
+	PFUN_CALLBACK m_pGoonNext{ nullptr };
+	// 回调函数的参数
+	void *m_pArgsFrist{ nullptr };
+	void *m_pArgsNext{ nullptr };
 };
